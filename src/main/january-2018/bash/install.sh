01cb6678ce46c571be746a9b96dbd6763130eed4
danglotb <bdanglot@gmail.com>
2018-04-02 12:08:03 +0200

    add lib and retrieve cvc4 in installation

--- src/main/january-2018/bash/install.sh
+++ src/main/january-2018/bash/install.sh
@@ -9,3 +9,7 @@ git clone https://github.com/STAMP-project/Ex2Amplifier.git
 cd Ex2Amplifier
 ~/apache-maven-3.3.9/bin/mvn install -DskipTests
 cd ..
+
+wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.4-x86_64-linux-opt
+{ echo "catg.cvc4Command=" ; echo $(pwd); echo "/" ; echo $(ls | grep cvc4); } | sed ':a;N;s/\n//;ba' >> lib/catg.conf
+chmod +x cvc4-1.4-x86_64-linux-opt

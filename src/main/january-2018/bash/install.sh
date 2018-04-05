b7df659050052737c3874fcb82ae3b2c4b754033
danglotb <bdanglot@gmail.com>
2018-04-05 09:46:30 +0200

    update dependencies to deployed dspot

--- src/main/january-2018/bash/install.sh
+++ src/main/january-2018/bash/install.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/STAMP-project/dspot.git
-cd dspot
-~/apache-maven-3.3.9/bin/mvn install -DskipTests
-cd ..
+# this script is used on Clusty, the cluster of Inria.
+# this is why we have ~/apache-maven-3.3.9/bin/mvn to reach maven
 
+# cloning Ex2Amplifier
 git clone https://github.com/STAMP-project/Ex2Amplifier.git
 cd Ex2Amplifier
 ~/apache-maven-3.3.9/bin/mvn install -DskipTests
 cd ..
 
+# getting cvc4
 wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.4-x86_64-linux-opt
 { echo "catg.cvc4Command=" ; echo $(pwd); echo "/" ; echo $(ls | grep cvc4); } | sed ':a;N;s/\n//;ba' >> lib/catg.conf
 chmod +x cvc4-1.4-x86_64-linux-opt
+
+# building experiments project
+~/apache-maven-3.3.9/bin/mvn install -DskipTests

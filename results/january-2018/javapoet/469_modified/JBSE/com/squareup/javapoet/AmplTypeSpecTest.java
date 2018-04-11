6ce4fcf06b56cbee872a868b1d584c779ca3d7be
danglotb <benjamin.danglot@inria.fr>
2018-04-11 17:08:02 +0200

    add new result on javapoet

--- results/january-2018/javapoet/469_modified/JBSE/com/squareup/javapoet/AmplTypeSpecTest.java
+++ results/january-2018/javapoet/469_modified/JBSE/com/squareup/javapoet/AmplTypeSpecTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static Util.DEFAULT;
+
 
 @RunWith(JUnit4.class)
 public final class AmplTypeSpecTest {
@@ -25,29 +27,29 @@ public final class AmplTypeSpecTest {
     }
 
     private boolean isJava8() {
-        return (Util.DEFAULT) != null;
+        return (DEFAULT) != null;
     }
 
     private String toString(TypeSpec typeSpec) {
-        return JavaFile.builder(tacosPackage, typeSpec).build().toString();
+        return build().toString();
     }
 
     @Test(timeout = 10000)
     public void annotationWithFields() throws Exception {
         FieldSpec field = FieldSpec.builder(int.class, "FOO").addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).initializer("$L", 101).build();
-        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec)((com.squareup.javapoet.FieldSpec.Builder)((com.squareup.javapoet.FieldSpec)field).toBuilder()).build()).toString());
-        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec)field).toString());
-        Assert.assertEquals(-449343922, ((int) (((com.squareup.javapoet.FieldSpec)field).hashCode())));
-        Assert.assertEquals(-449343922, ((int) (((com.squareup.javapoet.FieldSpec)((com.squareup.javapoet.FieldSpec.Builder)((com.squareup.javapoet.FieldSpec)field).toBuilder()).build()).hashCode())));
+        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec) (build())).toString());
+        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec) (field)).toString());
+        Assert.assertEquals((-449343922), ((int) (((com.squareup.javapoet.FieldSpec) (field)).hashCode())));
+        Assert.assertEquals((-449343922), ((int) (((com.squareup.javapoet.FieldSpec) (build())).hashCode())));
         TypeSpec anno = TypeSpec.annotationBuilder("Anno").addField(field).build();
-        Assert.assertEquals(1023254950, ((int) (((com.squareup.javapoet.TypeSpec)((com.squareup.javapoet.TypeSpec.Builder)((com.squareup.javapoet.TypeSpec)anno).toBuilder()).build()).hashCode())));
-        Assert.assertEquals("@interface Anno {\n  int FOO = 101;\n}\n", ((com.squareup.javapoet.TypeSpec)anno).toString());
-        Assert.assertEquals("@interface Anno {\n  int FOO = 101;\n}\n", ((com.squareup.javapoet.TypeSpec)((com.squareup.javapoet.TypeSpec.Builder)((com.squareup.javapoet.TypeSpec)anno).toBuilder()).build()).toString());
-        Assert.assertEquals(1023254950, ((int) (((com.squareup.javapoet.TypeSpec)anno).hashCode())));
-        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec)((com.squareup.javapoet.FieldSpec.Builder)((com.squareup.javapoet.FieldSpec)field).toBuilder()).build()).toString());
-        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec)field).toString());
-        Assert.assertEquals(-449343922, ((int) (((com.squareup.javapoet.FieldSpec)field).hashCode())));
-        Assert.assertEquals(-449343922, ((int) (((com.squareup.javapoet.FieldSpec)((com.squareup.javapoet.FieldSpec.Builder)((com.squareup.javapoet.FieldSpec)field).toBuilder()).build()).hashCode())));
+        Assert.assertEquals(1023254950, ((int) (((com.squareup.javapoet.TypeSpec) (build())).hashCode())));
+        Assert.assertEquals("@interface Anno {\n  int FOO = 101;\n}\n", ((com.squareup.javapoet.TypeSpec) (anno)).toString());
+        Assert.assertEquals("@interface Anno {\n  int FOO = 101;\n}\n", ((com.squareup.javapoet.TypeSpec) (build())).toString());
+        Assert.assertEquals(1023254950, ((int) (((com.squareup.javapoet.TypeSpec) (anno)).hashCode())));
+        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec) (build())).toString());
+        Assert.assertEquals("public static final int FOO = 101;\n", ((com.squareup.javapoet.FieldSpec) (field)).toString());
+        Assert.assertEquals((-449343922), ((int) (((com.squareup.javapoet.FieldSpec) (field)).hashCode())));
+        Assert.assertEquals((-449343922), ((int) (((com.squareup.javapoet.FieldSpec) (build())).hashCode())));
     }
 }
 

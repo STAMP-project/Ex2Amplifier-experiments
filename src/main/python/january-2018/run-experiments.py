62b20b8ee4447d7f27c616b36bbbdb9cbbbceb07
danglotb <bdanglot@gmail.com>
2018-04-16 10:15:51 +0200

    slicing experiments, update to dspot 1.1.0

--- src/main/python/january-2018/run-experiments.py
+++ src/main/python/january-2018/run-experiments.py
@@ -3,7 +3,7 @@ import json
 import argparse
 
 
-def run(project, flags, onClusty=False, mustClone=True):
+def run(project, flags, onClusty=False, mustClone=True, index=-1):
     base_cmd_jar = ("~/jdk1.8.0_121/bin/" if onClusty else "") + \
                    "java -jar target/Ex2Amplifier-experiments-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
     date = "january-2018"
@@ -28,7 +28,9 @@ def run(project, flags, onClusty=False, mustClone=True):
     with open(path_to_json_project_file) as data_file:
         pull_request_data = json.load(data_file)["pullRequests"]
 
-    for pr_data in pull_request_data:
+
+    for i in ( (range(0, len(pull_request_data))) if index == -1 else [index]):
+        pr_data = pull_request_data[i]
         for mode in ["", "--reverse"]:
             for flag in flags:
                 print base_cmd_run, pr_data["id"], mode, flag
@@ -39,12 +41,15 @@ if __name__ == '__main__':
     flags = ["--amplifiers"] if "amplifiers" in sys.argv else ["--aampl", "", "--jbse"]
     onClusty = "onClusty" in sys.argv
     mustClone = "mustClone" in sys.argv
+    index = int(sys.argv[-1]) if sys.argv[-1].isdigit() else -1
 
     if len(sys.argv) < 2:
-        print "usage python run-experiments <project> [onClusty] [mustClone] [amplifiers]"
+        print "usage python run-experiments <project> [onClusty] [mustClone] [amplifiers] [indexOfPR]"
     else:
-        run(project=sys.argv[1],
+        run(
+            project=sys.argv[1],
             flags=flags,
             onClusty=onClusty,
-            mustClone=mustClone
+            mustClone=mustClone,
+            index=index
             )

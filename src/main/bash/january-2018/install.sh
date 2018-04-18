656645d26d50a6d5dacf49ea6c225fcbc497ad2d
danglotb <bdanglot@gmail.com>
2018-04-18 14:10:20 +0200

    udapte dspot version

--- src/main/bash/january-2018/install.sh
+++ src/main/bash/january-2018/install.sh
@@ -3,6 +3,13 @@
 # this script is used on Clusty, the cluster of Inria.
 # this is why we have ~/apache-maven-3.3.9/bin/mvn to reach maven
 
+# cloning DSpot
+git clone https://github.com/STAMP-project/dspot.git
+cd dspot
+~/apache-maven-3.3.9/bin/mvn install -DskipTests
+cd ..
+
+
 # cloning Ex2Amplifier
 git clone https://github.com/STAMP-project/Ex2Amplifier.git
 cd Ex2Amplifier

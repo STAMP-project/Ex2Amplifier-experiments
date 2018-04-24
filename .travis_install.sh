2ce186a40c7c49779d5f383d43b3c0f097e6b4eb
danglotb <bdanglot@gmail.com>
2018-04-24 13:12:26 +0200

    fix install

--- .travis_install.sh
+++ .travis_install.sh
@@ -5,8 +5,16 @@ cd dspot
 mvn clean install -DskipTests
 cd ..
 
+git clone http://github.com/danglotb/jbse -b pre-java8
+cd jbse
+mvn install -DskipTests
+
 git clone https://github.com/STAMP-project/Ex2Amplifier
 cd Ex2Amplifier
 mvn clean install -DskipTests
 
 cd ..
+
+wget http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.4-x86_64-linux-opt
+{ echo "catg.cvc4Command=" ; echo $(pwd); echo "/" ; echo $(ls | grep cvc4); } | sed ':a;N;s/\n//;ba' >> lib/catg.conf
+chmod +x cvc4-1.4-x86_64-linux-opt

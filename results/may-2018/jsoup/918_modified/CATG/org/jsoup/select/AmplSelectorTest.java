34abf2e039b8f9b8d4850ca732e3d0da44b93627
danglotb <benjamin.danglot@inria.fr>
2018-05-23 14:12:29 +0200

    add result jsoup 918

---
+++ results/may-2018/jsoup/918_modified/CATG/org/jsoup/select/AmplSelectorTest.java
@@ -0,0 +1,12 @@
+package org.jsoup.select;
+
+
+import org.jsoup.MultiLocaleRule;
+import org.junit.Rule;
+
+
+public class AmplSelectorTest {
+    @Rule
+    public MultiLocaleRule rule = new MultiLocaleRule();
+}
+

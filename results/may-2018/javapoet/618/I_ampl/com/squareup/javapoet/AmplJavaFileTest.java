8e49f3c86a79e3af70a34b04fb8824c3086f2cbf
danglotb <benjamin.danglot@inria.fr>
2018-06-11 14:51:13 +0200

    add result sbampl javapoet

--- results/may-2018/javapoet/618/I_ampl/com/squareup/javapoet/AmplJavaFileTest.java
+++ results/may-2018/javapoet/618/I_ampl/com/squareup/javapoet/AmplJavaFileTest.java
@@ -2,8 +2,10 @@ package com.squareup.javapoet;
 
 
 import com.google.common.truth.Truth;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.lang.model.element.Modifier;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +24,12 @@ public final class AmplJavaFileTest {
     private TypeSpec importStaticTypeSpec(String name) {
         MethodSpec method = MethodSpec.methodBuilder("minutesToSeconds").addModifiers(Modifier.PUBLIC, Modifier.STATIC).returns(long.class).addParameter(long.class, "minutes").addStatement("$T.gc()", System.class).addStatement("return $1T.SECONDS.convert(minutes, $1T.MINUTES)", TimeUnit.class).build();
         return TypeSpec.classBuilder(name).addMethod(method).build();
+    }
+
+    @Test(timeout = 300000)
+    public void annotatedTypeParamlitString13161litString13362() throws Exception {
+        String source = JavaFile.builder("    return SECONDS.convert(minutes, MINUTES);\n", TypeSpec.classBuilder("Taco").addField(ParameterizedTypeName.get(ClassName.get(List.class), ClassName.get("com.squareup.meat", "com.squareup.tacos").annotated(AnnotationSpec.builder(ClassName.get("com.squareup.tacos", "Spicy")).build())), "chorizo").build()).build().toString();
+        Assert.assertEquals("package     return SECONDS.convert(minutes, MINUTES);\n;\n\nimport com.squareup.meat.com.squareup.tacos;\nimport com.squareup.tacos.Spicy;\nimport java.util.List;\n\nclass Taco {\n  List<@Spicy com.squareup.tacos> chorizo;\n}\n", source);
     }
 }
 
